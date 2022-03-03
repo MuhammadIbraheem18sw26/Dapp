f33e484789f2a2641474e35990afb9f0a5b86dc7
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-03-03 19:37:57 +0500

    initialized the project

---
+++ android/app/src/main/java/com/ibraheem/dppblockchain/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ibraheem.dppblockchain;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/ibraheem/dppblockchain/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ibraheem.dppblockchain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
